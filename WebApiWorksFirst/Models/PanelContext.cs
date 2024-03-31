43c5c61060c5464cd79e6b788667e74d59a9297e
Arif Ozan AKTAŞ <aoaktas@hotmail.com>
2024-03-31 13:13:53 +0300

    Add project files.

---
+++ WebApiWorksFirst/Models/PanelContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiWorksFirst.Models;
+
+public class PanelContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DLOP8P5\SQLEXPRESS;Initial Catalog=WebApiPanel;Integrated Security=True;Trust Server Certificate=True;");
+    }
+
+    public DbSet<AdminUser> AdminUsers { get; set; }
+}
